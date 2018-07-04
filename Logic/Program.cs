7c74e5a29464b4ee2e2259c6064ccdb051b63f29
codex881 <pruetsapon.xuz@gmail.com>
2018-07-04 23:22:15 +0700

    add input data for calculate logic.

--- Logic/Program.cs
+++ Logic/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 using Logic.Model;
 
 namespace Logic
@@ -7,13 +10,34 @@ namespace Logic
     {
         static void Main(string[] args)
         {
-            int[] blocks = { 1,0,3,1,0,1,2,0,1,2 };
-            var countWater = WaterBlock.WaterBlockCalculator(blocks);
-            Console.WriteLine("Water in block : " + countWater);
+            Console.WriteLine("Please select the choice : [1-2]");
+            Console.WriteLine("1. InputText");
+            Console.WriteLine("2. WaterBlock");
 
-            var inputText = "az/11w*-6a#!b";
-            var countNumber = InputText.TextCalculator(inputText, true, true);
-            Console.WriteLine("Count Number : " + countNumber);
+            var choice = Console.ReadLine();
+            switch(Int32.Parse(choice))
+            {
+                case 1:
+                    Console.WriteLine("Please input text.");
+                    var inputText = Console.ReadLine();
+                    var countNumber = InputText.TextCalculator(inputText, true, true);
+                    Console.WriteLine("Count Number : " + countNumber);
+                    break;
+                case 2:
+                    var regex = new Regex(@"[\d]");
+                    Console.WriteLine("Please input number.");
+                    var blocks = new List<int>();
+                    do {
+                        var input = Console.ReadLine();
+                        blocks.Add(regex.IsMatch(input) == false ? 0 : Int32.Parse(input));
+                    } while(blocks.Count < 5);
+                    var countWater = WaterBlock.WaterBlockCalculator(blocks.ToArray());
+                    Console.WriteLine("Water in block : " + countWater);
+                    break;
+                default:
+                    Console.WriteLine("Please input [1-2]");
+                    break;
+            }
         }
     }
 }
